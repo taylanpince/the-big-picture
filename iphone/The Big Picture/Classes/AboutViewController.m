18ad4e1bdc15a34c5e19c02fb7df4f87db247c3b
Taylan Pince <taylanpince@gmail.com>
2009-06-28 15:41:12 -0400

    Renamed app in the about view

--- iphone/The Big Picture/Classes/AboutViewController.m
+++ iphone/The Big Picture/Classes/AboutViewController.m
@@ -28,7 +28,7 @@
 		
 		UIButton *appButton = [UIButton buttonWithType:UIButtonTypeCustom];
 		
-		[appButton setTitle:@"The Big Picture v1.0.0RC1\nby Hippo Foundry →" forState:UIControlStateNormal];
+		[appButton setTitle:@"Big Picture v1.0.0RC2\nby Hippo Foundry →" forState:UIControlStateNormal];
 		[appButton.titleLabel setFont:[UIFont systemFontOfSize:12.0]];
 		[appButton.titleLabel setTextColor:[UIColor whiteColor]];
 		[appButton.titleLabel setNumberOfLines:0];
